6e9729c15fd8a1fe324fb94c1e7191b639faf31a
Cristóbal Fresno Rodríguez <cristobalfresno@gmail.com>
2021-10-11 08:32:24 +0000

    Posterior FASTQC is working

---
+++ 1_Filter_by_Quality/003-fastqc/config.mk
@@ -0,0 +1,7 @@
+# FASTQC usable executable version
+#
+# Using system version `sudo apt install fatsqc`
+# FASTQC="fastqc" 
+#
+# Using FASTQC version from the web installed in software directory
+FASTQC="../../softwares/FastQC/fastqc" 
