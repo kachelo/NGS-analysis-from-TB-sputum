c4891b0b86daf4720073d1a0561a71d2fe692d52
Cristóbal Fresno Rodríguez <cristobalfresno@gmail.com>
2021-10-23 23:06:46 +0000

    Adding GATK H37RV variants data base

--- 3_Variant_calling_by_GATK/009-Variant_analysis/config.mk
+++ 3_Variant_calling_by_GATK/009-Variant_analysis/config.mk
@@ -1,19 +1,19 @@
 #PATH to executables. Should include java and other dependencies as directed by README.md of the pipeline.
 
 #PATH to call GATK jar file
-GATK="../../softwares/GenomeAnalysisTK-3.8-1-0-gf15c1c3ef/GenomeAnalysisTK.jar"
+GATK="../../../softwares/GenomeAnalysisTK-3.8-1-0-gf15c1c3ef/GenomeAnalysisTK.jar"
 
 ############Reference Genome fasta file
 ##Para 1-H37RV
-REF="../../reference/Mycobacterium_tuberculosis_H37Rv.fna"
+REF="../../../references/www.ncbi.nlm.nih.gov/nuccore/NC_000962.3/Mycobacterium_tuberculosis_H37Rv.fa"
 
 ############dbSNP
 ##Para 1-H37RV
-dbSNP="../../known-variants/known_final.vcf"
+dbSNP="../../../references/known_variants/known_final.vcf"
 
 ############Coordenadas 
 #Coordenadas de bed para correr para 1
-coordenadas="../../known-variants/Bed-IGV+100_NC_000962.3.bed"
+coordenadas="../../../references/known_variants/Bed-IGV+100_NC_000962.3.bed"
 
 #Number of threads for parallelization of GATK and compression by bgzip
 NT="3"
